faa92475bf238e2bdc437da18c3c7fbcf18b0df3
cmmerritt <christiane.merritt@gmail.com>
2021-03-25 15:56:04 -0700

    add button click event listener and actions

--- app.js
+++ app.js
@@ -2,6 +2,51 @@
 
 import { getRandomThrow } from './get-random-throw.js';
 
+import { didUserWin } from './did-user-win.js';
+
+const playButton = document.getElementById('play-button');
+const resetButton = document.getElementById('reset-button');
+
+const computerPickArea = document.getElementById('computer-pick');
+const resultArea = document.getElementById('result');
+const winsArea = document.getElementById('wins');
+const lossesArea = document.getElementById('losses');
+const drawsArea = document.getElementById('draws');
+const totalArea = document.getElementById('total');
+
+
 // initialize state
 
+let wins, losses, draws, total = 0;
+
 // set event listeners to update state and DOM
+
+playButton.addEventListener('click', () => {
+    const randomNum = Math.random() * 10;
+    const computerPick = getRandomThrow(randomNum);
+    const userSelectedRadioButton = document.querySelector('input:checked');
+    const userPick = userSelectedRadioButton.value;
+
+    computerPickArea.textContent = computerPick;
+    
+    total++;
+    totalArea.textContent = `${total}`;
+
+    const result = didUserWin(userPick, computerPick);
+    resultArea.textContent = `${result}`;
+
+    if (result === 'win') {
+        wins++;
+        winsArea.textContent = `${wins}`;
+    } else if (result === 'lose') {
+        losses++;
+        winsArea.textContent = `${losses}`;
+    } else if (result === 'draw') {
+        draws++;
+        drawsArea.textContent = `${draws}`;
+    }
+
+    //
+});
+
+

bee6c7ea28ae556351db920d08fbad69dc0d2c22
cmmerritt <christiane.merritt@gmail.com>
2021-03-25 17:10:10 -0700

    finish goals

--- app.js
+++ app.js
@@ -2,6 +2,51 @@
 
 import { getRandomThrow } from './get-random-throw.js';
 
+import { didUserWin } from './did-user-win.js';
+
+const playButton = document.getElementById('play-button');
+const resetButton = document.getElementById('reset-button');
+
+const computerPickArea = document.getElementById('computer-pick');
+const resultArea = document.getElementById('result');
+const winsArea = document.getElementById('wins');
+const lossesArea = document.getElementById('losses');
+const drawsArea = document.getElementById('draws');
+const totalArea = document.getElementById('total');
+
+
 // initialize state
 
+let wins = 0;
+let losses = 0;
+let draws = 0;
+
 // set event listeners to update state and DOM
+
+playButton.addEventListener('click', () => {
+    const randomNum = Math.random() * 10;
+    const computerPick = getRandomThrow(randomNum);
+    const userSelectedRadioButton = document.querySelector('input:checked');
+    const userPick = userSelectedRadioButton.value;
+
+    computerPickArea.textContent = computerPick;
+
+    const result = didUserWin(userPick, computerPick);
+    
+
+    if (result === 'win') {
+        wins++;
+        resultArea.textContent = 'You win!';
+    } else if (result === 'lose') {
+        losses++;
+        resultArea.textContent = 'You lose';
+    } else if (result === 'draw') {
+        draws++;
+        resultArea.textContent = 'It\'s a draw';
+    }
+
+    winsArea.textContent = wins;
+    drawsArea.textContent = draws;
+    lossesArea.textContent = losses;
+    totalArea.textContent = wins + draws + losses;
+});
--- did-user-win.js
+++ did-user-win.js
@@ -1,5 +1,23 @@
 export function didUserWin(playerChoice, computerChoice) {
     if (playerChoice === computerChoice) {
         return 'draw';
+    } else if (playerChoice === 'rock') {
+        if (computerChoice === 'scissors') {
+            return 'win';
+        } else if (computerChoice === 'paper') {
+            return 'lose';
+        }
+    } else if (playerChoice === 'paper') {
+        if (computerChoice === 'rock') {
+            return 'win';
+        } else if (computerChoice === 'scissors') {
+            return 'lose';
+        }
+    } else if (playerChoice === 'scissors') {
+        if (computerChoice === 'paper') {
+            return 'win';
+        } else if (computerChoice === 'rock') {
+            return 'lose';
+        }
     }
 }
